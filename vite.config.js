981ebcccbd7b754a733735c3ff6b456192d4985d
khadyyade <khady0804@gmail.com>
2026-08-05 15:54:54 +0100

    fix: base condicional para Vercel y Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/asociacion-win95/',
+  // Si estás desplegando en Vercel, la base será '/', de lo contrario (Pages), será '/asociacion-win95/'
+  base: process.env.VERCEL ? '/' : '/asociacion-win95/',
 })
